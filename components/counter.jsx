62272c547071bdd464fd8c7929e59a681f2a432c
deweyspencer <dewey.spencer@gmail.com>
2019-11-21 11:58:00 -0500

    Passing event arguments

--- components/counter.jsx
+++ components/counter.jsx
@@ -10,10 +10,6 @@ class Counter extends Component {
     this.handleIncrement = this.handleIncrement.bind(this);
   };
 
-  handleIncrement () {
-    this.setState({count:this.state.count + 1});
-  };
-
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += (this.state.count === 0) ? "warning" : "primary";
@@ -25,11 +21,17 @@ class Counter extends Component {
     return count === 0 ? "Zero" : count;
   };
 
+  handleIncrement (product) {
+    console.log(product);
+    this.setState({ count:this.state.count + 1 });
+  };
+
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={ () => this.handleIncrement({ id: 1 })} 
+          className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
