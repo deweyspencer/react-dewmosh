530e73dc2dc080a5a12bc22a01912a0ce2af116a
deweyspencer <dewey.spencer@gmail.com>
2019-11-21 12:36:03 -0500

    sync test

--- components/counters.jsx
+++ components/counters.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Counters extends Component {
     state = {  }
     render() { 
-        return ( <span></span> );
+        return ( <span>here</span> );
     }
 }
  
