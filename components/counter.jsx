a96267b50ec96fd34703b45f2f3ee37be8b2e073
deweyspencer <dewey.spencer@gmail.com>
2019-11-21 16:08:32 -0500

    Completed passing data to components

--- components/counter.jsx
+++ components/counter.jsx
@@ -2,28 +2,22 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-  };
-
-  constructor (){
-    super();
-    this.handleIncrement = this.handleIncrement.bind(this);
+    value: this.props.value
   };
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
+    classes += (this.state.value === 0) ? "warning" : "primary";
     return classes;
   };
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   };
 
-  handleIncrement (product) {
-    console.log(product);
-    this.setState({ count:this.state.count + 1 });
+  handleIncrement = ()=> {
+    this.setState({ value:this.state.value + 1 });
   };
 
   render() {
--- components/counters.jsx
+++ components/counters.jsx
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-    state = {  }
+    state = { 
+      counters: [
+        { id: 1, value: 2 },
+        { id: 2, value: 0 },
+        { id: 3, value: 3 },
+        { id: 4, value: 0 }
+      ]
+     }
+
     render() { 
-        return ( <div>
-            <Counter />
-            <Counter />
-            <Counter />
-        </div> );
+        return ( 
+          <div>
+            { this.state.counters.map(counter => 
+              <Counter count={3} key={counter.id} value={counter.value}/> )}
+          </div> 
+        );
     }
 }
  
