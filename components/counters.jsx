597b22b35f0ae10c3ccc8f2c05054f7f76108f9e
deweyspencer <dewey.spencer@gmail.com>
2019-11-21 16:17:01 -0500

    Example of passing children

--- components/counters.jsx
+++ components/counters.jsx
@@ -15,7 +15,10 @@ class Counters extends Component {
         return ( 
           <div>
             { this.state.counters.map(counter => 
-              <Counter count={3} key={counter.id} value={counter.value}/> )}
+              <Counter key={counter.id} value={counter.value}>
+                <h4>Counter #{counter.id}</h4>
+              </Counter>
+            )}
           </div> 
         );
     }
