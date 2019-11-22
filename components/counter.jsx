a5fd4b48858698f8436f4a3703e7036e78a74bfb
deweyspencer <dewey.spencer@gmail.com>
2019-11-22 16:42:48 -0500

    Move state up, single source of truth, and synchronization

--- components/counter.jsx
+++ components/counter.jsx
@@ -1,23 +1,16 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-  };
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += (this.state.value === 0) ? "warning" : "primary";
+    classes += (this.props.counter.value === 0) ? "warning" : "primary";
     return classes;
   };
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
-  };
-
-  handleIncrement = () => {
-    this.setState({ value:this.state.value + 1 });
   };
 
   render() {
@@ -26,7 +19,7 @@ class Counter extends Component {
         {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm"
-          onClick={this.handleIncrement}>
+          onClick={() => this.props.onIncrement(this.props.counter)}>
           Increment
         </button>
         <button className="btn btn-danger btn-sm- m-2"
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
 import Counters from './components/counters';
+import NavBar from './components/navbar';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
@@ -9,19 +9,51 @@ import './style.css';
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      name: 'React'
+  }
+
+  state = { 
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
     };
+
+  handleReset = () => {
+    const counters = this.state.counters.map( 
+      c => {
+        c.value = 0;
+        return c;
+      });
+    this.setState({ counters });
+  }
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = {...counter};
+    counters[index].value++;
+    this.setState({ counters });
   }
 
   render() {
     return (
-      <div>
-        <Counters />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
+      <React.Fragment>
+        <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length}/>
+        <main className="container">
+          <Counters counters={this.state.counters}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            onReset={this.handleReset}
+          />
+        </main>
+      </React.Fragment>
     );
   }
 }
